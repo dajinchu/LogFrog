943fe8404e82062eb9bb299b0bdd845fbeaca258
Da-Jin Chu <dajinchu@gmail.com>
2015-05-27 17:39:16 -0400

    Crap attempt to allow orientation to change but not actually change anything.

--- core/src/com/gmail/dajinchu/MainGame.java
+++ core/src/com/gmail/dajinchu/MainGame.java
@@ -2,36 +2,40 @@ package com.gmail.dajinchu;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MainGame extends ApplicationAdapter implements InputProcessor{
 	SpriteBatch batch;
 	private Model model;
     private ShapeRenderer renderer;
-    private OrthographicCamera cam;
+    private Viewport viewport;
 
-    float mapHeight=140f, mapWidth = 140f;
-
+    float nodeRadius=4, logWidth=4;
+    int mapWidth, mapHeight;
 
     Preferences prefs;
     int level;
+
     @Override
 	public void create () {
 		batch = new SpriteBatch();
         renderer = new ShapeRenderer();
-        float w = Gdx.graphics.getWidth();
-        float h = Gdx.graphics.getHeight();
-        cam = new OrthographicCamera(mapWidth * (w / h), mapHeight);
-        cam.rotate(90, 0, 0, 1);
-        cam.update();
+
+        mapWidth = (int) (4*20+nodeRadius*2);
+        mapHeight = (int) (6*20+nodeRadius*2);
+        viewport = new ExtendViewport(mapWidth, mapHeight);
+
+        //cam.rotate(90, 0, 0, 1);
 
         prefs=Gdx.app.getPreferences("My Prefs");
         level = prefs.getInteger("level",1);
@@ -42,12 +46,12 @@ public class MainGame extends ApplicationAdapter implements InputProcessor{
 
 	@Override
 	public void render () {
-        cam.update();
-        renderer.setProjectionMatrix(cam.combined);
-        batch.setProjectionMatrix(cam.combined);
+        renderer.setProjectionMatrix(viewport.getCamera().combined);
+        batch.setProjectionMatrix(viewport.getCamera().combined);
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		renderer.begin(ShapeRenderer.ShapeType.Filled);
+        renderer.rect(0,0,mapWidth,mapHeight);
         for(Link l:model.links){
             switch (l.state){
                 case CONNECTED:renderer.setColor(Color.YELLOW);break;
@@ -55,7 +59,7 @@ public class MainGame extends ApplicationAdapter implements InputProcessor{
                 case POTENTIAL:renderer.setColor(Color.LIGHT_GRAY);break;
             }
             if(l.selected)renderer.setColor(Color.MAROON);
-            renderer.rectLine(l.n1.x*20,l.n1.y*20,l.n2.x*20,l.n2.y*20,4);
+            renderer.rectLine(l.n1.x*20,l.n1.y*20,l.n2.x*20,l.n2.y*20,logWidth);
         }
         renderer.setColor(Color.LIGHT_GRAY);
         for(Node n:model.nodes.values()){
@@ -65,7 +69,7 @@ public class MainGame extends ApplicationAdapter implements InputProcessor{
                 renderer.setColor(Color.DARK_GRAY);
             }
             if(n.id==model.nodes.size-1)renderer.setColor(Color.GREEN);
-            renderer.circle(n.x*20,n.y*20,4);
+            renderer.circle(n.x*20,n.y*20,nodeRadius);
         }
         renderer.end();
         }
@@ -73,10 +77,19 @@ public class MainGame extends ApplicationAdapter implements InputProcessor{
     @Override
     public void resize(int w, int h){
         Gdx.app.log("Main", w+" "+h);
-        cam.viewportWidth = mapHeight*w/h;
-        cam.viewportHeight = mapWidth;
-        cam.position.set(50, mapHeight / 2f, 0);
-        cam.update();
+        if(Gdx.input.getNativeOrientation().equals(Input.Orientation.Portrait)){
+
+            viewport.update(w,h,true);
+            viewport.setWorldSize();
+            viewport.getCamera().up.set(0, 1, 0);
+            viewport.getCamera().rotate(-Gdx.input.getRotation(), 0, 0, 1);
+            Gdx.app.log("Main","landscape"+Gdx.input.getRotation());
+        }else {
+            viewport.update(w, h, true);
+        }
+        viewport.getCamera().translate(-4,-4,0);
+        viewport.getCamera().update();
+
     }
 
     @Override
@@ -96,7 +109,7 @@ public class MainGame extends ApplicationAdapter implements InputProcessor{
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        Vector3 v = cam.unproject(new Vector3(screenX,screenY,0));
+        Vector3 v = viewport.unproject(new Vector3(screenX,screenY,0));
         Rectangle clickBox = new Rectangle(v.x,v.y,1,1);
         Link clickedLink = null;
 
