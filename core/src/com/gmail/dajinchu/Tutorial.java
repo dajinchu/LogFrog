9af359ad389ef67b7a511bdf874a4202867fc22a
Da-Jin Chu <dajinchu@gmail.com>
2015-07-08 18:15:04 -0400

    Fixed tutorial getting in loop

--- core/src/com/gmail/dajinchu/Tutorial.java
+++ core/src/com/gmail/dajinchu/Tutorial.java
@@ -119,6 +119,7 @@ public class Tutorial extends MainGame{
                 isArrow=false;
             }
         }else{
+            sm.prefs.putInteger("level", 0);
             sm.mainmenu();
             return;
         }
